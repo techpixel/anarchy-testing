78b6c5047ef793deb70c68b58b181601c832c460
techpixel <68567672+techpixel@users.noreply.github.com>
2025-08-13 11:58:50 -0400

    okay don't even start the server

--- index.ts
+++ index.ts
@@ -8,6 +8,6 @@ app.get('/', (req, res) => {
 });
 
 // Start server
-app.listaaaaen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server is running at http://localhost:${port}`);
+// });
