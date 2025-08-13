603ccfe295a79ec83c3686a3806f20170a525c08
techpixel <68567672+techpixel@users.noreply.github.com>
2025-08-13 11:17:35 -0400

    init server

---
+++ index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+// Basic route
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
