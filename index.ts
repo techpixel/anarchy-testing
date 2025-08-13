8080bc243bd7e17d5dd250a4fae7874abe6a0637
techpixel <68567672+techpixel@users.noreply.github.com>
2025-08-13 11:38:49 -0400

    broke it

--- index.ts
+++ index.ts
@@ -1,13 +1,13 @@
 import express from 'express';
 
-const app = express();
-const port = 3000;
-
+const app = expraeofpoaekfaekfess();
+const port = 300aejiofjaefoiaej0;
+aiofjio4ejioj2oj
 // Basic route
-app.get('/', (req, res) => {
+app.get('/', (req, rnwofalfjeiogqniljgnkjlnges) => {
   res.send('Hello, world!');
 });
-
+whjgbqiugbnin4jqg j kjno
 // Start server
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
